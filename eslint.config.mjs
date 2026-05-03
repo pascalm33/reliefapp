b1ef2a21301389223b363449e807bc25ccb3eec7
Pascal MARIA <pascal.maria@aviv-group.com>
2026-05-03 20:56:51 +0200

    Fix dev and production builds

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 
 const config = [
-  globalIgnores(["coverage/**", "playwright-report/**", "test-results/**"]),
+  globalIgnores([".open-next/**", "coverage/**", "playwright-report/**", "test-results/**"]),
   ...nextVitals
 ];
 
